287702baec7a7504949445d3e89c71cbc8306e58
thientran <thien.tran@saigontechnology.vn>
2017-09-27 21:11:37 +0700

    Initial Commit

---
+++ cai_cho_swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cai_cho_swift
+//
+//  Created by Thien Tran on 9/27/17.
+//  Copyright © 2017 Thien Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
