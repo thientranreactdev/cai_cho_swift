3be6359570cffcc3a9437c4b2ba298c19767fc7d
trquth <tranquthien@gmail.com>
2017-09-30 08:34:57 +0700

    Complex view with NSLayoutContraint

--- cai_cho_swift/ViewController.swift
+++ cai_cho_swift/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     var containerView : UIView!
     var firstLabel : UILabel!
     var secondLabel : UILabel!
+    var firstButton : UIButton!
+    var secondButton : UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +30,49 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        centerView()
+        complexView()
+    }
+    
+    func complexView() {
+        containerView = UIView()
+        containerView.backgroundColor = UIColor.blue
+        containerView.translatesAutoresizingMaskIntoConstraints = false;
+        
+        self.view.addSubview(containerView);
+        
+        let centerHorizontally =   NSLayoutConstraint(item: containerView, attribute: .centerX, relatedBy: .equal, toItem: self.view, attribute: .centerX, multiplier: 1, constant: 0)
+        let centerVertically = NSLayoutConstraint(item: containerView, attribute: .centerY, relatedBy: .equal, toItem: self.view, attribute: .centerY, multiplier: 1, constant: 0)
+        let height = NSLayoutConstraint(item: containerView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 150)
+        let width = NSLayoutConstraint(item: containerView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 200)
+        self.view.addConstraints([centerHorizontally, centerVertically, height, width])
+        
+        firstButton = UIButton(type: .custom)
+        firstButton.backgroundColor = UIColor.orange
+        firstButton.setTitle("@x1", for: .normal)
+        firstButton.setTitleColor(.white, for: .normal)
+        firstButton.translatesAutoresizingMaskIntoConstraints = false
+        containerView.addSubview(firstButton)
+        
+        let centerHorizontallyOfFistButton =  NSLayoutConstraint(item: firstButton, attribute: .centerX, relatedBy: .equal, toItem: containerView, attribute: .centerX, multiplier: 1, constant: 0)
+        let centerVerticallyOfFirstButton = NSLayoutConstraint(item: firstButton, attribute: .centerY, relatedBy: .equal, toItem: containerView, attribute: .centerY, multiplier: 1, constant: 1)
+        let topOfFirstButton = NSLayoutConstraint(item: firstButton, attribute: .top, relatedBy: .equal, toItem: containerView, attribute: .top, multiplier: 1, constant: 10)
+        let widthOfFirstButton = NSLayoutConstraint(item: firstButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 50)
+        let heightOfFirstButton = NSLayoutConstraint(item: firstButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 30)
+        containerView.addConstraints([centerHorizontallyOfFistButton,topOfFirstButton,widthOfFirstButton, heightOfFirstButton])
+        
+        secondButton = UIButton(type: .custom)
+        secondButton.backgroundColor = UIColor.red
+        secondButton.setTitle("@x2", for: .normal)
+        secondButton.setTitleColor(.white, for: .normal)
+        secondButton.translatesAutoresizingMaskIntoConstraints = false
+        containerView.addSubview(secondButton)
+        
+        NSLayoutConstraint(item: secondButton, attribute: .centerX, relatedBy: .equal, toItem: firstButton, attribute: .centerX, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: secondButton, attribute: .top, relatedBy: .equal, toItem: firstButton, attribute: .bottom, multiplier: 1, constant: 10).isActive = true
+        NSLayoutConstraint(item: secondButton, attribute: .width, relatedBy: .equal, toItem: firstButton, attribute: .width, multiplier: 2, constant: 0).isActive = true
+        NSLayoutConstraint(item: secondButton, attribute: .height, relatedBy: .equal, toItem: firstButton, attribute: .height, multiplier: 1, constant: 0).isActive = true
+        
+        
     }
     
     func centerView() {
@@ -41,7 +85,7 @@ class ViewController: UIViewController {
         let centerVertically = NSLayoutConstraint(item: secondView, attribute: .centerY, relatedBy: .equal, toItem: self.view, attribute: .centerY, multiplier: 1, constant: 0)
         
         let height = NSLayoutConstraint(item: secondView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 100)
-                let width = NSLayoutConstraint(item: secondView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 100)
+        let width = NSLayoutConstraint(item: secondView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 100)
         
         NSLayoutConstraint.activate([centerVertically, centerHorizontally,width, height])
         
