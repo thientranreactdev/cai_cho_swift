fbd323062d9e134bc925ec37fcf7036878fdc001
trquth <tranquthien@gmail.com>
2017-10-05 13:12:38 +0700

    Step 1: Show list cell

---
+++ cai_cho_swift/HomeController.swift
@@ -0,0 +1,39 @@
+//
+//  HomeController.swift
+//  cai_cho_swift
+//
+//  Created by mac on 10/5/17.
+//  Copyright © 2017 Thien Tran. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Home";
+        collectionView?.backgroundColor = UIColor.white;
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5;
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath);
+        cell.backgroundColor = UIColor.red;
+        return cell;
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
+
+}
