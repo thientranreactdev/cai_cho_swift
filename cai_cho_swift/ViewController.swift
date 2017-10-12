916ef15d8072b2b4e822334ea564d197b63121f3
thientran <thien.tran@saigontechnology.vn>
2017-10-12 21:37:00 +0700

    Declare button

--- cai_cho_swift/ViewController.swift
+++ cai_cho_swift/ViewController.swift
@@ -12,13 +12,52 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(firstButton)
+        view.addSubview(secondButton)
+        view.addSubview(thirdButton)
+        view.addSubview(fourthButton)
+        
+        setupConstraint()
+    }
+    
+    let firstButton : UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Hello World", for: .normal)
+        return button
+    }()
+    let secondButton : UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("🇻🇳", for: .normal)
+        return button
+    }()
+    let thirdButton : UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("😍", for: .normal)
+        return button
+    }()
+    let fourthButton : UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("🎩", for: .normal)
+        return button
+    }()
+    
+    func setupConstraint() {
+        firstButton.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        firstButton.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        firstButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
