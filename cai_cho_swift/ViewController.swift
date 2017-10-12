df2be38b8434cdaa7fd9c00d5d47608c55ce410a
trquth <tranquthien@gmail.com>
2017-10-12 10:54:29 +0700

    Add image view on topImageView

--- cai_cho_swift/ViewController.swift
+++ cai_cho_swift/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        view.addSubview(bearImageView)
+       // view.addSubview(bearImageView)
         view.addSubview(descriptionTextView)
         setupLayout()
     }
@@ -27,8 +27,9 @@ class ViewController: UIViewController {
     
     let descriptionTextView : UITextView = {
         let textView = UITextView()
-        textView.text = "Join us today in our fun and games!"
-        textView.font = UIFont.boldSystemFont(ofSize: 18)
+        let attributeText = NSMutableAttributedString(string: "Join us today in our fun and games!", attributes: [NSAttributedStringKey.font : UIFont.boldSystemFont(ofSize: 18)])
+        attributeText.append(NSMutableAttributedString(string:"\n\n\nAre you ready for loads and loads of fun? Don't wait any longer! we hope to see you in our store soon.", attributes:[NSAttributedStringKey.font: UIFont.systemFont(ofSize: 14),NSAttributedStringKey.foregroundColor: UIColor.gray]))
+        textView.attributedText = attributeText
         textView.translatesAutoresizingMaskIntoConstraints = false
         textView.textAlignment = .center
         textView.isEditable = false
@@ -38,24 +39,23 @@ class ViewController: UIViewController {
     
     private func setupLayout(){
         let topImageContainerView = UIView()
-        topImageContainerView.backgroundColor = .yellow
         view.addSubview(topImageContainerView)
         topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
         topImageContainerView.topAnchor.constraint(equalTo:view.topAnchor).isActive = true
-        topImageContainerView.leftAnchor.constraint(equalTo:view.leftAnchor).isActive = true
-        topImageContainerView.rightAnchor.constraint(equalTo:view.rightAnchor).isActive = true
+        topImageContainerView.leadingAnchor.constraint(equalTo:view.leadingAnchor).isActive = true
+        topImageContainerView.trailingAnchor.constraint(equalTo:view.trailingAnchor).isActive = true
+        //        topImageContainerView.leftAnchor.constraint(equalTo:view.leftAnchor).isActive = true
+        //        topImageContainerView.rightAnchor.constraint(equalTo:view.rightAnchor).isActive = true
         topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
         
+        topImageContainerView.addSubview(bearImageView)
+        bearImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
+        bearImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
+        bearImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier:0.5).isActive = true
         
-        
-        bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        bearImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 150).isActive = true
-        bearImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        bearImageView.heightAnchor.constraint(equalToConstant:200).isActive = true
-        
-        descriptionTextView.topAnchor.constraint(equalTo: bearImageView.bottomAnchor, constant:120).isActive = true
-        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor).isActive = true
+        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor, constant:25).isActive = true
+        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant:-25).isActive = true
         descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
         
     }
