cddb8198da98aadb339ae535747394ea9df8dcc7
thientran <thien.tran@saigontechnology.vn>
2017-10-17 23:09:55 +0700

    90% like the design UI

--- cai_cho_swift/ViewController.swift
+++ cai_cho_swift/ViewController.swift
@@ -39,6 +39,36 @@ class ViewController: UIViewController {
         return button
     }()
     
+    let welcomeLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Welcome to Grapevine"
+        label.font = UIFont.boldSystemFont(ofSize: 15)
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let summaryLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Communicate with relatives, friends and colleagues anywhere, anytime."
+        label.font = label.font.withSize(12)
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let paggingControl : UIImageView = {
+        let image = UIImageView(image:#imageLiteral(resourceName: "NavigationDots"))
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    let appImage : UIImageView = {
+        let image = UIImageView(image:#imageLiteral(resourceName: "CommunicationIcon"))
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
     func setUpView(){
         let color = UIColor(red: 42/255, green: 103/255, blue: 58/255, alpha: 1)
         view.backgroundColor = color
@@ -47,9 +77,22 @@ class ViewController: UIViewController {
         view.addSubview(appName)
         view.addSubview(skipButton)
         
-        let icon_constraint_V = NSLayoutConstraint.constraints(withVisualFormat: "V:|-20-[icon(30)]", options: [], metrics: nil, views: ["icon": appIcon])
-        let app_name_constraint_V = NSLayoutConstraint.constraints(withVisualFormat: "V:|-20-[appName]", options: [], metrics: nil, views: ["appName": appName])
-        let skip_constraint_V = NSLayoutConstraint.constraints(withVisualFormat: "V:|-20-[skipButton]", options: [], metrics: nil, views: ["skipButton": skipButton])
+        view.addSubview(appImage)
+        view.addSubview(welcomeLabel)
+        view.addSubview(summaryLabel)
+        view.addSubview(paggingControl)
+        
+        let views = ["icon" : appIcon,
+                     "appName" : appName,
+                     "skipButton" : skipButton,
+                     "image":appImage,
+                     "welcome": welcomeLabel,
+                     "summary": summaryLabel,
+                     "pagging": paggingControl]
+        
+        let icon_constraint_V = NSLayoutConstraint.constraints(withVisualFormat: "V:|-20-[icon(30)]", options: [], metrics: nil, views: views)
+        let app_name_constraint_V = NSLayoutConstraint.constraints(withVisualFormat: "V:|-23-[appName]", options: [], metrics: nil, views: views)
+        let skip_constraint_V = NSLayoutConstraint.constraints(withVisualFormat: "V:|-20-[skipButton]", options: [], metrics: nil, views: views)
         
         view.addConstraints(icon_constraint_V)
         view.addConstraints(app_name_constraint_V)
@@ -58,6 +101,28 @@ class ViewController: UIViewController {
         let top_header_constraint_H = NSLayoutConstraint.constraints(withVisualFormat: "H:|-10-[icon(30)]-[appName]-[skipButton]-10-|", options: [], metrics: nil, views: ["icon": appIcon, "appName": appName, "skipButton": skipButton])
         
         view.addConstraints(top_header_constraint_H)
+        
+        let app_image_constraint_V = NSLayoutConstraint.constraints(withVisualFormat: "V:[icon]-10-[image]", options: [], metrics: nil, views: views)
+        
+        view.addConstraints(app_image_constraint_V)
+        
+        let welcome_constraint_H  = NSLayoutConstraint.constraints(withVisualFormat: "H:|-10-[welcome]-10-|", options: [], metrics: nil, views: views)
+        let summary_constraint_H = NSLayoutConstraint.constraints(withVisualFormat: "H:|-10-[summary]-10-|", options: [], metrics: nil, views: views)
+        
+        view.addConstraints(welcome_constraint_H)
+        view.addConstraints(summary_constraint_H)
+        
+        let welcome_with_image_constraint_V = NSLayoutConstraint.constraints(withVisualFormat: "V:[image]-10-[welcome]", options: [.alignAllCenterX], metrics: nil, views: views)
+        
+        view.addConstraints(welcome_with_image_constraint_V)
+        
+        let welcome_with_summary_constraint_V = NSLayoutConstraint.constraints(withVisualFormat: "V:[welcome]-10-[summary]", options: [.alignAllLeading, .alignAllTrailing], metrics: nil, views: views)
+        
+        view.addConstraints(welcome_with_summary_constraint_V)
+        
+        let paging_constraint_V = NSLayoutConstraint.constraints(withVisualFormat: "V:[summary]-15-[pagging(10)]", options: [.alignAllCenterX], metrics: nil, views: views)
+        
+        view.addConstraints(paging_constraint_V)
         
     }
 
