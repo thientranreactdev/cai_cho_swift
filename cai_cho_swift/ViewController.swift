ca570d91529ee2287d7c558ce9fe3f15b32f455c
thientran <thien.tran@saigontechnology.vn>
2017-10-27 19:31:59 +0700

    Init project

--- cai_cho_swift/ViewController.swift
+++ cai_cho_swift/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //Todo: Start project
     }
 
     override func didReceiveMemoryWarning() {
